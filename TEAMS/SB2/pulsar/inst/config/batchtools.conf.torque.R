5977d003c761656b863db4369e0d36f78d580a76
Pande Putu Erawijantari <perawija@atlas.giu.fi>
2023-04-24 15:34:23 +0300

    remove teams

--- TEAMS/SB2/pulsar/inst/config/batchtools.conf.torque.R
+++
@@ -1,4 +0,0 @@
-cluster.functions = makeClusterFunctionsTORQUE(
-                      template = pulsar::findTemplateFile('simpletorque'),
-                      scheduler.latency = 1,
-                      fs.latency = 65)
